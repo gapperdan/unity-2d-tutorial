9b4ca08ebaa08895ef6155f3008c2dc476a094c0
Danny Tizon <dtizon@gmail.com>
2016-03-10 21:40:34 -0800

    initial commit

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+	[SerializeField] private MemoryCard originalCard;
+	[SerializeField] private Sprite[] images;
+
+	void Start() {
+		int id = Random.Range(0, images.Length);
+		originalCard.SetCard(id, images[id]);
+	}
+}
